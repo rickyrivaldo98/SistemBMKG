96be179e372a837a1015372f5a7ef516d1dd8a82
darknowns <doublezero13@gmail.com>
2020-02-02 19:05:32 +0700

    uwuwuwu

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -6,6 +6,7 @@ class Admin extends CI_Controller
 
     public function __construct()
     {
+        parent::__construct();
         $this->load->model('CSVModel');
         $this->load->model('modelresponden');
         $this->load->helper('url');
