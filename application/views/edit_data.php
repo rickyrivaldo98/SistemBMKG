ca0e18609db22c857c10c3eb5495173308285982
darknowns <doublezero13@gmail.com>
2020-06-28 11:22:12 +0700

    Anas Revisi Sensei

--- application/views/edit_data.php
+++ application/views/edit_data.php
@@ -71,18 +71,18 @@
                                         <label for="Bulan">Bulan*</label>
                                         <select class="form-control select2bs4 <?php echo form_error('Bulan') ? 'is-invalid' : '' ?>" name="Bulan" tabindex="-1">
                                             <option value="<?php echo $data_hujan->Bulan ?>"><?php echo $data_hujan->Bulan ?></option>
-                                            <option value="Januari">Januari</option>;
-                                            <option value="Februari">Februari</option>;
-                                            <option value="Maret">Maret</option>;
-                                            <option value="April">April</option>;
-                                            <option value="Mei">Mei</option>;
-                                            <option value="Juni">Juni</option>;
-                                            <option value="Juli">Juli</option>;
-                                            <option value="Agustus">Agustus</option>;
-                                            <option value="September">September</option>;
-                                            <option value="Oktober">Oktober</option>;
-                                            <option value="November">November</option>;
-                                            <option value="Desember">Desember</option>;
+                                            <option value="1">Januari</option>;
+                                            <option value="2">Februari</option>;
+                                            <option value="3">Maret</option>;
+                                            <option value="4">April</option>;
+                                            <option value="5">Mei</option>;
+                                            <option value="6">Juni</option>;
+                                            <option value="7">Juli</option>;
+                                            <option value="8">Agustus</option>;
+                                            <option value="9">September</option>;
+                                            <option value="10">Oktober</option>;
+                                            <option value="11">November</option>;
+                                            <option value="12">Desember</option>;
                                         </select>
                                         <div class="invalid-feedback">
                                             <?php echo form_error('Bulan') ?>
